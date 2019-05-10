ecb36796ea56fa6cdeffb964ec5e320669e75a63
Maulik <shahms95@gmail.com>
2019-05-10 10:13:58 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 wget https://repo.anaconda.com/archive/Anaconda2-2019.03-Linux-x86_64.sh;
 bash Anaconda2-2019.03-Linux-x86_64.sh;
 source ~/.bashrc;
-conda install -c conda-forge hyperopt ;
+# conda install -c conda-forge hyperopt ;
 conda install -c conda-forge tensorflow ;
